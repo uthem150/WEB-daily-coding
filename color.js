e7975cc5b8afd9ebaa248db06f344aa556a2aae5
uthem150 <142042011+uthem150@users.noreply.github.com>
2024-01-25 03:51:26 +0900

    cdn을 통해, java script파일 jQuery적용
    구글 cdn사용

--- color.js
+++ color.js
@@ -5,15 +5,19 @@ var Links = {
     for (var i = 0; i < alist.length; i++) {
       alist[i].style.color = color;
     }
+
+    // $("a").color("color", color);
   },
 };
 //Body라는 변수에 객체를 담을 것임
 var Body = {
   setColor: function (color) {
-    document.querySelector("body").style.color = color;
+    // document.querySelector("body").style.color = color;
+    $("body").css("color", color);
   },
   setBackgroundColor: function (color) {
-    document.querySelector("body").style.backgroundColor = color;
+    // document.querySelector("body").style.backgroundColor = color;
+    $("body").css("backgroundColor", color);
   },
 };
 function nightDayHandler(self) {
